bd2c139fa6d3929fd33f1cb43ba81644ad90613f
Kushnir2205 <kushnirroman22@gmail.com>
2023-07-03 11:44:49 +0300

    add callback funk

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -5,22 +5,13 @@ import styles from './App.module.css';
 import { Filter } from 'components/Filter/Filter.jsx';
 
 const App = () => {
-  const [contacts, setContacts] = useState([
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ]);
-  const [filter, setFilter] = useState('');
-
-  useEffect(() => {
+  const [contacts, setContacts] = useState(() => {
     const contactsData = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contactsData) ?? [];
+    return parsedContacts;
+  });
 
-    if (parsedContacts) {
-      setContacts(parsedContacts);
-    }
-  }, []);
+  const [filter, setFilter] = useState('');
 
   useEffect(() => {
     localStorage.setItem('contacts', JSON.stringify(contacts));
